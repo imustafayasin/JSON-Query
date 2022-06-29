013ed698904b50dd11d01e6fbd1413a98fa490cf
imustafayasin <imustafayasin@gmail.com>
2022-06-29 22:35:30 +0300

    Add editor

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template lang="pug">
 .grid
-  .g-col-6.border  Col 1
-  .g-col-6.border Col 2
+  .g-col-6
+    h4(class="ms-2 my-2") Input
+    .editor(ref="input" data-placeholder="\n //Add Json Here :)")
+  .g-col-6
+    h4(class="ms-2 my-2") Query
+    .editor(ref="query" data-placeholder="//You can use your json as input \ninput")
 
-  .g-col-6.border Col 3
-  .g-col-6.border Col 4
+  .g-col-6
+    h4(class="ms-2 my-2") Output
+    .editor(ref="output" )
+  .g-col-6
+    h4(class="ms-2 my-2") Setting
+    .settings.p-3 setting
 </template>
 
 <script>
+import * as monaco from "monaco-editor";
+
 export default {
   name: "App",
+  mounted() {
+    console.log(Object.entries(this.$refs));
+    for (let editor_element of Object.values(this.$refs)) {
+      monaco.editor.create(editor_element, {
+        value: `${editor_element.dataset.placeholder}`,
+        language: "javascript",
+        lineNumbers: "on",
+        roundedSelection: false,
+        scrollBeyondLastLine: false,
+        readOnly: false,
+        theme: "vs-dark",
+      });
+    }
+  },
 };
 </script>
 
@@ -18,19 +42,31 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
+html,
+body {
+  width: 100%;
+  padding: 10px;
+}
+
 .grid {
-  height: 100vh;
   display: grid;
   grid-template-columns: 1fr 1fr;
+  gap: 20px;
 
-  div {
-    height: 100%;
-    display: grid;
-    place-items: center;
+  .g-col-6 {
+
+    .editor {
+      width: 100%;
+      min-height: 350px;
+
+      .monaco-editor {
+        width: 100% !important;
+        height: 100% !important;
+      }
+    }
   }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.ttf$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [new MonacoWebpackPlugin()]
 });
